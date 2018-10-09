dad36d009156a2e2e48e20420f401132465dbf2d
TALHA AYAR <talhaayar@TALHA-MacBook-Air.local>
2018-10-09 09:22:27 +0300

    Initial Commit

---
+++ Yemektarifleri/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Yemektarifleri
+//
+//  Created by TALHA AYAR on 9.10.2018.
+//  Copyright © 2018 TALHA AYAR. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
